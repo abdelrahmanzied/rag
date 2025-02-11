55b72db4022619c6a8e74928788e0ccb2c50ec80
Abdelrahman Abozied <abdelrahmanzied@gmail.com>
2025-02-12 00:01:03 +0200

    Add Config settings

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routers.base import base_router
 from dotenv import load_dotenv
+import os
 
-load_dotenv(".env")
+# load_dotenv(".env")
 
+# load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 app = FastAPI()
 
--- routers/base.py
+++ routers/base.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 import os
+
+from helpers.config import Settings, get_settings
 
 base_router = APIRouter(
     prefix="/v1",
@@ -7,11 +9,9 @@ base_router = APIRouter(
 )
 
 @base_router.get('/health')
-async def health():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_version')
+async def health(app_settings: Settings = Depends(get_settings)):
     return {
         'msg': "Hello Worlds from health router",
-        'app_name': app_name,
-        'app_version': app_version,
+        'app_name': app_settings.APP_NAME,
+        'app_version': app_settings.APP_VERSION,
     }
